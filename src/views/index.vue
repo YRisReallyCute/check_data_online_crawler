556e763f2d7f8dcdbe8d9cdb37ad61e7d1401c95
YRisReallyCute <970518722@qq.com>
2019-09-23 17:30:45 +0800

    first commit all

---
+++ src/views/index.vue
@@ -0,0 +1,19 @@
+<!--<template>-->
+<!--    <div>-->
+<!--      <hello-word></hello-word>-->
+<!--    </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--  import HelloWord from '../components/HelloWorld'-->
+<!--  export default {-->
+<!--    name: 'index',-->
+<!--    components: {-->
+<!--      HelloWord-->
+<!--    }-->
+<!--  }-->
+<!--</script>-->
+
+<!--<style scoped>-->
+
+<!--</style>-->
